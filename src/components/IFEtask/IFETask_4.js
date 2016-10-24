32cf8b145bcc852f4217b6583bc301952acc7be3
Bizpies <bizpies@Bizpiess-MBP.lan>
2016-10-24 13:20:14 +0800

    add function

--- src/components/IFEtask/IFETask_4.js
+++ src/components/IFEtask/IFETask_4.js
@@ -1,24 +1,51 @@
 require('styles/IFETask.scss');
 import React from 'react';
 var IFETask_4 = React.createClass({
+	getInitialState: function() {
+		return {
+			aqiDataArr:[]//空气质量数组，{city:'',value:0} 
+		};
+	},
+	addData(){
+		var aqiData=[{
+				city:this.refs.city.value,
+				value:this.refs.aqi.value
+			}],
+			newData=this.state.aqiDataArr.concat(aqiData);
+		this.refs.city.value='';
+		this.refs.aqi.value='';
+		this.setState({
+			aqiDataArr: newData
+		});
+
+	},
+	delete(item){
+		var arr=[];
+		arr=this.state.aqiDataArr;
+		arr.splice(item,1)
+		this.setState({
+			aqiDataArr: arr
+		});
+	},
 	render: function() {
+		var aqiArr=this.state.aqiDataArr.map((value,item)=>{
+			if(value.city){
+				return (
+					<tr key={item} >
+						<td>{value.city}</td><td>{value.value}</td><td><button onClick={()=>this.delete(item)}>删除</button></td>
+					</tr>
+				)
+			}
+		})
 		return (
 			<div>
 				<div>
-				<label>城市名称：<input id="aqi-city-input" type="text"/></label><br/>
-					<label>空气质量指数：<input id="aqi-value-input" type="text"/></label><br/>
-					<button id="add-btn">确认添加</button>
+				<label>城市名称：<input id="aqi-city-input" type="text" ref="city"/></label><br/>
+					<label>空气质量指数：<input id="aqi-value-input" type="text" ref="aqi"/></label><br/>
+					<button id="add-btn" onClick={this.addData}>确认添加</button>
 				</div>
 				<table id="aqi-table">
-					<tr>
-						<td>城市</td><td>空气质量</td><td>操作</td>
-					</tr>
-					<tr>
-						<td>北京</td><td>90</td><td><button>删除</button></td>
-					</tr>
-					<tr>
-						<td>北京</td><td>90</td><td><button>删除</button></td>
-					</tr>
+					{aqiArr}
 				</table>
 			</div>
 		);
