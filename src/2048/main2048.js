68876f9a432dfe175af4f4fbfa499b115512346b
Bizpies <bizpies@Bizpiess-MacBook-Pro.local>
2016-11-27 17:06:02 +0800

    增加移动判断

--- src/2048/main2048.js
+++ src/2048/main2048.js
@@ -4,6 +4,7 @@ var score = 0;
 $(document).ready(function() {
     //页面加载完毕执行newgame函数
     newgame();
+    $('#newgamebtn').bind('click',newgame);
 })
 
 //新游戏
@@ -17,6 +18,7 @@ function newgame() {
 
 //初始化棋盘格
 function init() {
+
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
 
@@ -45,7 +47,9 @@ function updateBoardView() {
 	
 	$('.number-cell').remove();
 	for (var i = 0 ; i < 4 ; i ++){
+
 		 for(var j =0;j<4; j++){
+
 		 	$('#grid-container').append('<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>');
 		 	var theNumberCell=$('#number-cell-'+i+'-'+j);
 
@@ -89,9 +93,139 @@ function generateOneNumber() {
 
 	//随机一个数字
 	var randNumber =Math.random() < 0.5 ? 2 : 4 ;
-	console.log(board,renderX,renderY)
 	//在随机位置显示随机数字
 	board[renderX][renderY]=randNumber;
-	updateBoardView();
+	animation.showNumWithAnimation(renderX,renderY,randNumber);
 	return true;
+}
+$(document).keydown(function (event) {
+	event.preventDefault()
+	switch(event.keyCode){
+		case 37://left
+			if(moveLeft()){
+				generateOneNumber();
+				isGameOver();
+			}
+			break;
+		case 38://up
+			if(moveUp()){
+				generateOneNumber();
+				isGameOver();
+			}
+			break;
+		case 39://right
+			if(moveRight()){
+				generateOneNumber();
+				isGameOver();
+			}
+			break;
+		case 40://down
+			if(moveDown()){
+				generateOneNumber();
+				isGameOver();
+			}
+			break;
+		default ://default
+			break;
+	}
+})
+//左移
+function moveLeft() {
+
+	if(! support.canMoveLeft(board)){
+		return false 
+	}
+	for (var i=0;i<4;i++){
+		for (var j=1;j<4;j++){
+			if(board[i][j]!=0){
+				for(var k=0;k<j;k++){
+					if(board[i][k]==0&&support.noBlockHorizontal(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(i,j,i,k);
+						board[i][k]=board[i][j];
+						board[i][j]=0;
+						continue
+					}else if(board[i][k]==board[i][j]&&support.noBlockHorizontal(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(i,j,i,k);
+						
+						//add
+						board[i][k]+=board[i][j];
+						board[i][j]=0;
+						continue
+					}
+				}
+			}
+		}
+	}
+	setTimeout(updateBoardView,200)
+	return true
+}
+//上移
+function moveUp() {
+
+	if(! support.canMoveUp(board)){
+		return false 
+	}
+	for (var i=0;i<4;i++){
+		for (var j=1;j<4;j++){
+			if(board[j][i]!=0){
+				for(var k=0;k<j;k++){
+					if(board[k][i]==0&&support.noBlockVertical(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(j,i,k,i);
+						board[k][i]=board[j][i];
+						board[j][i]=0;
+						continue
+					}else if(board[k][i]==board[j][i]&&support.noBlockVertical(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(j,i,k,i);
+						
+						//add
+						board[k][i]+=board[j][i];
+						board[i][j]=0;
+						continue
+					}
+				}
+			}
+		}
+	}
+	setTimeout(updateBoardView,200)
+	return true
+}
+//右移
+function moveRight() {
+
+	if(! support.canMoveRight(board)){
+		return false 
+	}
+	for (var i=0;i<4;i++){
+		for (var j=0;j<3;j++){
+			if(board[i][j]!=0){
+				for(var k=0;k<j;k++){
+					if(board[i][k]==0&&support.noBlockHorizontal(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(i,j,i,k);
+						board[i][k]=board[i][j];
+						board[i][j]=0;
+						continue
+					}else if(board[i][k]==board[i][j]&&support.noBlockHorizontal(i,k,j,board)){
+						//move
+						animation.showMoveAnimation(i,j,i,k);
+						
+						//add
+						board[i][k]+=board[i][j];
+						board[i][j]=0;
+						continue
+					}
+				}
+			}
+		}
+	}
+	setTimeout(updateBoardView,200)
+	return true
+}
+//判断是否游戏结束
+function isGameOver() {
+	
 }
