011937d58b611efe60adb126f147f43c148e658a
Joe <joelin@airag.cn>
2017-07-13 00:11:53 +0800

    路由切换至 browserHistory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './router';
 
 ReactDOM.render((
-    <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
         {routes}
     </Router>
 ), document.getElementById('app'));
