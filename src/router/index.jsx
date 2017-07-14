77232ec74b939bf5ded20ce8f43b1b568030fed4
leon <lz_n10828@163.com>
2017-07-14 11:45:33 +0800

    隐藏内容

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../pages/App/App';
 
-const Home = (location, callback) => {
-    require.ensure([], require => {
-        callback(null, require('../pages/Home/Home').default)
-    },'Home')
-};
-const Portfolio = (location, callback) => {
-    require.ensure([], require => {
-        callback(null, require('../pages/Portfolio/Portfolio').default)
-    },'Portfolio')
-};
-const About = (location, callback) => {
-    require.ensure([], require => {
-        callback(null, require('../pages/About/About').default)
-    },'About')
-};
+// const Home = (location, callback) => {
+//     require.ensure([], require => {
+//         callback(null, require('../pages/Home/Home').default)
+//     },'Home')
+// };
+// const Portfolio = (location, callback) => {
+//     require.ensure([], require => {
+//         callback(null, require('../pages/Portfolio/Portfolio').default)
+//     },'Portfolio')
+// };
+// const About = (location, callback) => {
+//     require.ensure([], require => {
+//         callback(null, require('../pages/About/About').default)
+//     },'About')
+// };
 export default (
     <Route path="/" component={App}>
     {/* <Route path="/" component={Test}> */}
-        <IndexRoute getComponent={Home}/>
-        {/*<Route path="blog" component={Blog}/>*/}
-        <Route path="portfolio" getComponent={Portfolio}/>
-        <Route path="about" getComponent={About}/>
+        {/*<IndexRoute getComponent={Home}/>*/}
+        {/*/!*<Route path="blog" component={Blog}/>*!/*/}
+        {/*<Route path="portfolio" getComponent={Portfolio}/>*/}
+        {/*<Route path="about" getComponent={About}/>*/}
     </Route>
 )
