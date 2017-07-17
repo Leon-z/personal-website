14465bdd44c4c9d260ace0ed0cd4b1b86cd333a7
leon <lz_n10828@163.com>
2017-07-17 11:22:04 +0800

    test

--- cfg/dist.js
+++ cfg/dist.js
@@ -11,7 +11,7 @@ let BowerWebpackPlugin = require('bower-webpack-plugin');
 let config = Object.assign({}, baseConfig, {
     entry: {
         main: path.join(__dirname, '../src/index'),
-        vendor: ['react','react-dom']
+        vendor: []
     },
     // entry: path.join(__dirname, '../src/index'),
     output: {
