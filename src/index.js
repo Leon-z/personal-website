8a6d0736d46827f05519a924ac798bc1936f619f
leon <lz_n10828@163.com>
2017-07-17 11:46:33 +0800

    定位bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './router';
+// import { Router, browserHistory } from 'react-router';
+// import routes from './router';
 
 ReactDOM.render((
-    <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
-        {routes}
-    </Router>
+    <div>测试</div>
 ), document.getElementById('app'));
