38afe2ea452c9aba18a06cc75de7097eefe24eaa
leon <lz_n10828@163.com>
2017-07-17 11:58:07 +0800

    调整插件顺序

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Router, browserHistory } from 'react-router';
-// import routes from './router';
+import { Router, browserHistory } from 'react-router';
+import routes from './router';
 
 ReactDOM.render((
-    <div>测试</div>
+    <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
+        {routes}
+    </Router>
 ), document.getElementById('app'));
