e2f1632d844edff52a5151749f2fa14f63bd1ce1
leon <lz_n10828@163.com>
2017-07-26 18:49:27 +0800

    ADD gzip压缩

--- cfg/dist.js
+++ cfg/dist.js
@@ -6,6 +6,9 @@ let baseConfig = require('./base');
 let defaultSettings = require('./defaults');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
+const {UglifyJsPlugin} = webpack.optimize; // 压缩js
+const {CommonsChunkPlugin} = webpack.optimize; // 压缩js
+const CompressionWebpackPlugin = require('compression-webpack-plugin');//gzip 压缩
 // Add needed plugins here
 
 let config = Object.assign({}, baseConfig, {
@@ -29,7 +32,16 @@ let config = Object.assign({}, baseConfig, {
         new BowerWebpackPlugin({
             searchResolveModulesDirectories: false
         }),
-        new webpack.optimize.UglifyJsPlugin({
+        new CommonsChunkPlugin({
+            name: "vendor",
+            filename: 'js/[name].[chunkhash:8].chunk.js'
+        }),
+        new CommonsChunkPlugin({
+            name: "manifest",
+            minChunks: Infinity,
+            filename: 'js/[name].[chunkhash:8].chunk.js'
+        }),
+        new UglifyJsPlugin({
             compress: {
                 warnings: false
             },
@@ -38,18 +50,15 @@ let config = Object.assign({}, baseConfig, {
             },
             mangle: false
         }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendor",
-            filename: 'js/[name].[chunkhash:8].chunk.js'
+        new CompressionWebpackPlugin({ //gzip 压缩
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: new RegExp(
+                '\\.(js|css)$'    //压缩 js 与 css
+            ),
+            threshold: 10240,
+            minRatio: 0.8
         }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "manifest",
-            minChunks: Infinity,
-            filename: 'js/[name].[chunkhash:8].chunk.js'
-        }),
-        // new webpack.optimize.OccurenceOrderPlugin(),
-        // new webpack.optimize.AggressiveMergingPlugin(),
-        // new webpack.NoErrorsPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: 'body' // Inject all scripts into the body
